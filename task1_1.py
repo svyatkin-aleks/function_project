a7cbc940f0a8559f4bea940204e8ccef5fc86fc8
svyatkin <svyatkin1@MacBook-Pro-Macbook.local>
2021-01-27 20:05:31 +0300

    new function

---
+++ task1_1.py
@@ -0,0 +1,4 @@
+def my_function(a):
+    for i in range(len(a)):
+        print(f'Hello, {a[i]}')
+my_function(['Aleksei', 'Maria', 'Sasha', 'Petya', 'Andrei'])
---
+++ test.py
@@ -0,0 +1,6 @@
+def my_func(a,b):
+    summ = a + b
+    print(f'{a} + {b} = {summ}')
+    return summ
+a = my_func(4, 5)
+print(a)
