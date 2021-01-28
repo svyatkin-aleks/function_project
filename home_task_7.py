b05daa6a5b0a64ac3026e073620e917716f17f27
svyatkin <svyatkin1@MacBook-Pro-Macbook.local>
2021-01-28 12:58:52 +0300

    done first task

---
+++ home_task_7.py
@@ -0,0 +1,36 @@
+def inch_to_cm(a):
+    cm = a * 2.54
+    return cm
+def cm_to_inch(a):
+    inch = a / 2.54
+    return inch
+def mi_to_km(a):
+    km = a * 1.609
+    return km
+def km_to_mi(a):
+    mi = a / 1.609
+    return mi
+def lb_to_kg(a):
+    kg = a / 2.2046
+    return kg
+def kg_to_lb(a):
+    lb = a * 2.2046
+    return lb
+def oz_to_g(a):
+    g = a / 0.035274
+    return g
+def g_to_oz(a):
+    oz = a * 0.035274
+    return oz
+def gal_to_l(a):
+    l = a * 3.785
+    return l
+def l_to_gal:
+    gal = a / 3.785
+    return gal
+def pint_to_l(a):
+    l = a / 2.113
+    return l
+def l_to_pint(a):
+    pint = a * 2.113
+    return pint
